eb83ecd72e786fc1b32ff2d133bfa00c6db775ac
Dylan Deshler <deshler.dylan@gmail.com>
2025-07-21 00:42:30 -0400

    vae steps

--- neural/llama_train_exp5.py
+++ neural/llama_train_exp5.py
@@ -52,7 +52,7 @@ wandb_project = out_dir #'zinc20++'
 wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 1 # used to simulate larger batch sizes
+gradient_accumulation_steps = 2 # used to simulate larger batch sizes
 batch_size = 2 * 12# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 rate = 16000
