549a78799540343ce8ae46c8699f8dea83d6f5f7
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-06 20:45:23 -0400

    save_samples

--- neural/llama_train_exp6.py
+++ neural/llama_train_exp6.py
@@ -238,6 +238,17 @@ def get_lr(it):
     return min_lr + coeff * (learning_rate - min_lr)
 
 
+def save_samples(xs, ys, step):
+    batch_dir = os.path.join(out_dir, str(step))
+    os.makedirs(batch_dir, exist_ok=True)
+
+    for i in range(8):
+        x, y = xs[i].squeeze(), ys[i].squeeze()
+
+        # save .wavs
+        sf.write(os.path.join(batch_dir, f'{i}_real.wav'), x, rate)
+        sf.write(os.path.join(batch_dir, f'{i}_recon.wav'), y, rate)
+
 # logging
 if wandb_log and master_process:
     import wandb
@@ -277,9 +288,9 @@ while True:
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and master_process:
         X = get_batch('test')
-        # with ctx:
-        #     logits, loss = model(X)
-        # save_samples(X.cpu().detach().numpy(), logits.cpu().detach(), iter_num)
+        with ctx:
+            logits, loss = model(X)
+        save_samples(X.cpu().detach().numpy(), logits.cpu().detach(), iter_num)
         losses = estimate_loss()
         print(f"step {iter_num}: train loss {losses['train']:.6f}, val loss {losses['val']:.6f}")
         if wandb_log:
