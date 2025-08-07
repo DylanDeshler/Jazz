e8d820ee3398da30dabee75ec0a96f2d234180f8
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-06 21:45:08 -0400

    eval iters

--- neural/llama_train_exp6.py
+++ neural/llama_train_exp6.py
@@ -40,7 +40,7 @@ import glob
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'tokenizer9'
-eval_interval = 1000
+eval_interval = 2000
 log_interval = 100
 eval_iters = 400
 eval_only = False # if True, script exits right after the first eval
