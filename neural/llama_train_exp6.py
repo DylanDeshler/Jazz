65947a46905dda3a35b899677b358006f426b9ad
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-11 01:36:18 -0400

    single gpu

--- neural/llama_train_exp6.py
+++ neural/llama_train_exp6.py
@@ -52,8 +52,8 @@ wandb_project = out_dir #'zinc20++'
 wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 2 # used to simulate larger batch sizes
-batch_size = 32# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
+gradient_accumulation_steps = 1 # used to simulate larger batch sizes
+batch_size = 40# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 rate = 16000
 n_samples = rate
