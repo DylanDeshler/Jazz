5487cfb8509952cfddab79eca1dd8aed24638341
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-13 16:08:18 -0400

    recon

--- neural/fm.py
+++ neural/fm.py
@@ -89,7 +89,6 @@ class FM:
                 uncond_net_kwargs = {}
             uncond_pred = net(x_t, t=t * self.timescale, **uncond_net_kwargs)
             pred = uncond_pred + guidance * (pred - uncond_pred)
-            print(t, pred.min(), pred.mean(), pred.std(), pred.max())
         return pred
     
     def convert_sample_prediction(self, x_t, t, pred):
