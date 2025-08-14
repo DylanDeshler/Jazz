d6498ce5f744cdee539dadf35f337120bfc61cda
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-14 00:08:15 -0400

    recon

--- neural/fm.py
+++ neural/fm.py
@@ -150,7 +150,7 @@ class FMEulerSampler:
 
         with torch.no_grad():
             for i in range(n_steps):
-                t = t_steps[i].repeat(x_t.shape[0])
+                t = t_steps[i].repeat(x.shape[0])
                 x_t, noise = self.diffusion.add_noise(x, t)
                 neg_v = self.diffusion.get_prediction(
                     net,
