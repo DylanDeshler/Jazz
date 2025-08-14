726a40c0ca2e1abc1f77774ee99ca9f64508419c
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-14 00:03:57 -0400

    recon

--- neural/fm.py
+++ neural/fm.py
@@ -145,20 +145,29 @@ class FMEulerSampler:
         noise=None,
     ):
         device = next(net.parameters()).device
-        x_t = torch.randn(shape, device=device) if noise is None else noise
+        # x_t = torch.randn(shape, device=device) if noise is None else noise
+        x_t, noise = self.add_noise(x, t)
         t_steps = torch.linspace(1, 0, n_steps + 1, device=device)
 
         with torch.no_grad():
             for i in range(n_steps):
                 t = t_steps[i].repeat(x_t.shape[0])
-                neg_v = self.diffusion.get_reconstruction_prediction(
+                neg_v = self.diffusion.get_prediction(
                     net,
-                    x,
                     x_t,
                     t,
                     net_kwargs=net_kwargs,
                     uncond_net_kwargs=uncond_net_kwargs,
                     guidance=guidance,
                 )
+                # neg_v = self.diffusion.get_reconstruction_prediction(
+                #     net,
+                #     x,
+                #     x_t,
+                #     t,
+                #     net_kwargs=net_kwargs,
+                #     uncond_net_kwargs=uncond_net_kwargs,
+                #     guidance=guidance,
+                # )
                 x_t = x_t + neg_v * (t_steps[i] - t_steps[i + 1])
         return x_t
