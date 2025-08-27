96052432adf80f8296c8f81b23c2d50ae70041f6
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-27 02:30:50 -0400

    lam

--- neural/train_lam_dit.py
+++ neural/train_lam_dit.py
@@ -160,7 +160,7 @@ if init_from == 'scratch':
 elif init_from == 'resume':
     print(f"Resuming training from {out_dir}")
     # resume training from a checkpoint.
-    ckpt_path = os.path.join(out_dir, '84000_ckpt.pt')
+    ckpt_path = os.path.join(out_dir, '80000_ckpt.pt')
     checkpoint = torch.load(ckpt_path, map_location=device)
     model_args = checkpoint['model_args']
 
