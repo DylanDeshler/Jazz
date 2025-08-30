59fbd166f810f8a8b270faa69c3069c169851b09
Dylan Deshler <deshler.dylan@gmail.com>
2025-08-30 00:36:01 -0400

    lam

--- neural/train_lam_dit3.py
+++ neural/train_lam_dit3.py
@@ -212,7 +212,7 @@ def estimate_loss():
         for k in tqdm(range(eval_iters * gradient_accumulation_steps)):
             X = get_batch(split)
             with ctx:
-                loss = model(X, force_mask=True)
+                loss = model(X)
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
