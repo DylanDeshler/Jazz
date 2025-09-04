8da5ba58be3e7b335915c3a54cad8029fb79bdf0
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-04 13:51:47 -0400

    tokenizer2

--- neural/train_high_dito.py
+++ neural/train_high_dito.py
@@ -47,7 +47,7 @@ save_interval = eval_interval * 10
 eval_iters = 100
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = False # disabled by default
 wandb_project = out_dir #'zinc20++'
