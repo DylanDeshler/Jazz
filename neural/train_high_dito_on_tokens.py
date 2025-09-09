06608d73415a8dfe5a30ad8ddef3fcc25a7e2480
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-09 18:50:09 -0400

    tokenizer4

--- neural/train_high_dito_on_tokens.py
+++ neural/train_high_dito_on_tokens.py
@@ -41,7 +41,7 @@ import glob
 # I/O
 out_dir = 'tokenizer_high_tokens1'
 eval_interval = 1000
-log_interval = 10
+log_interval = 100
 save_interval = eval_interval * 10
 eval_iters = 100
 eval_only = False # if True, script exits right after the first eval
