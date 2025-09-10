371f7c509267e4e6dd0a0c4e4bcc233a44e64b3e
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-10 12:04:18 -0400

    tokenizer4

--- neural/consistency_decoder.py
+++ neural/consistency_decoder.py
@@ -390,7 +390,9 @@ class DownsampleV3(nn.Module):
         self.shortcut = PixelUnshuffleChannelAveragingDownSampleLayer(in_channels, out_channels, ratio)
     
     def forward(self, x):
-        print(x.shape, self.conv(x).shape, self.shortcut(x).shape)
+        print(x.shape)
+        print(self.conv(x).shape)
+        print(self.shortcut(x).shape)
         x = self.conv(x) + self.shortcut(x)
         return x
 
