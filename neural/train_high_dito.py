c6bc5bd0d1483166db578ccc52a4e61f3d499685
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-10 11:58:14 -0400

    tokenizer4

--- neural/train_high_dito.py
+++ neural/train_high_dito.py
@@ -187,8 +187,7 @@ def average_checkpoints(paths):
             acc[k] = acc[k].to(dtypes0[k])
     return acc
 
-model_args = dict(z_shape=(256, 50), n_residual_layers=3, lstm=0, transformer=1, dimension=256, n_filters=32, ratios=[8, 4, 4, 4, 2, 2], channels=[128, 256, 384, 512, 640, 768], dilation_base=3, down_proj=1024)
-# model_args = dict(z_shape=(256, 50), n_residual_layers=2, lstm=0, transformer=1, dimension=256, n_filters=64, ratios=[8, 5, 5, 4, 2], c0=192, c1=384, c2=768)
+model_args = dict(z_shape=(256, 50), n_residual_layers=3, lstm=0, transformer=1, dimension=256, n_filters=32, ratios=[8, 4, 4, 4, 2, 2], channels=[128, 256, 512, 512, 1024, 1024], dilation_base=3, down_proj=1024)
 if init_from == 'scratch':
     # init a new model from scratch
     print("Initializing a new model from scratch")
