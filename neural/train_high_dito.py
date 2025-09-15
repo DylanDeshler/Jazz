d0810c8f2339d4ee576fe9f68a998ed10f6bcbb4
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-15 16:30:17 -0400

    tokenizer5

--- neural/train_high_dito.py
+++ neural/train_high_dito.py
@@ -322,8 +322,8 @@ while True:
         param_group['lr'] = lr
     
     if iter_num == step1 or local_iter_num == 0 and iter_num >= step1:
-        # batch_size = 32
-        gradient_accumulation_steps *= 2
+        batch_size = 32
+        # gradient_accumulation_steps *= 2
     # if iter_num == step2 or local_iter_num == 0 and iter_num >= step2:
     #     batch_size = 48
     # if iter_num == step3 or local_iter_num == 0 and iter_num >= step3:
