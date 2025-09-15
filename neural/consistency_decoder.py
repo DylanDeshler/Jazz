d474025565805c72e2c5e7efb165b98957e215e3
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-15 16:41:45 -0400

    tokenizer5

--- neural/consistency_decoder.py
+++ neural/consistency_decoder.py
@@ -787,6 +787,7 @@ class DylanDecoderUNet2(nn.Module):
             x = mid(x, c)
 
         skips.pop()
+        print(x.shape, c.shape)
         for i, up in enumerate(reversed(self.up)):
             for block in up:
                 if isinstance(block, UpsampleV3):
@@ -795,6 +796,7 @@ class DylanDecoderUNet2(nn.Module):
                     x = torch.cat([x, skips.pop()], dim=1)
                     x = self.skip_projs[-i](x)
                 else:
+                    print(x.shape, c.shape)
                     x = block(x, c)
 
         return self.output(x)
