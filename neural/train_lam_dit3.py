8c7a5d188e2021467cf06494ed5ce617b4fcf078
Dylan Deshler <deshler.dylan@gmail.com>
2025-09-22 20:21:50 -0400

    lam

--- neural/train_lam_dit3.py
+++ neural/train_lam_dit3.py
@@ -46,7 +46,7 @@ save_interval = 10000
 eval_iters = 100
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = False # disabled by default
 wandb_project = out_dir #'zinc20++'
