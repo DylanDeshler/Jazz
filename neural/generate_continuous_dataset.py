58a1f7a22270167b93cc4ebff02a2f23218b7f5d
Dylan Deshler <dylan.deshler@gmail.com>
2025-10-10 13:52:40 -0400

    dataset

--- neural/generate_continuous_dataset.py
+++ neural/generate_continuous_dataset.py
@@ -46,134 +46,136 @@ print(len(paths))
 with open('/home/dylan.d/research/music/Jazz/song_instruments.json', 'r') as f:
     song_instruments = json.load(f)
 
-test = False
+if False:
 
-write_idx = 0
-write_paths = []
-total_write_batches = 48
+    test = False
 
-all_codes = []
-all_instruments = []
-with torch.no_grad():
-    for idx, path in enumerate(tqdm(paths)):
-        instruments = song_instruments[path.split('/')[-1]]
-        this_codes = []
-        if test:
-            this_samples = []
-        x, sr = librosa.load(path, sr=None)
+    write_idx = 0
+    write_paths = []
+    total_write_batches = 48
 
-        n_cuts = len(x) // n_samples
-        for i in range(n_cuts // batch_size):
-            batch = torch.from_numpy(np.stack([x[(i*batch_size+j)*n_samples:(i*batch_size+j+1)*n_samples] for j in range(batch_size)], axis=0)).unsqueeze(1).pin_memory().to(device, non_blocking=True)
-            _, codes = model.encode(batch)
-            codes = codes.permute(0, 2, 1).cpu().detach().numpy()
+    all_codes = []
+    all_instruments = []
+    with torch.no_grad():
+        for idx, path in enumerate(tqdm(paths)):
+            instruments = song_instruments[path.split('/')[-1]]
+            this_codes = []
             if test:
-                # will introduce padding but thats fine for encode only
-                x_, z = model.encode(batch)
-                y = model.decode(z, x_.shape)
-                this_samples.append(y.cpu().detach().numpy())
-            this_codes.append(codes)
-        
-        # remainder = len(x) - n_cuts * n_samples
-        i = n_cuts // batch_size
-        remainder = n_cuts - i * batch_size
-        # print(i, remainder, n_cuts, i * batch_size)
-        if remainder > 0:
-            batch = torch.from_numpy(np.stack([x[(i*batch_size+j)*n_samples:(i*batch_size+j+1)*n_samples] for j in range(remainder)], axis=0)).unsqueeze(1).pin_memory().to(device, non_blocking=True)
-            _, codes = model.encode(batch)
-            codes = codes.permute(0, 2, 1).cpu().detach().numpy()
+                this_samples = []
+            x, sr = librosa.load(path, sr=None)
+
+            n_cuts = len(x) // n_samples
+            for i in range(n_cuts // batch_size):
+                batch = torch.from_numpy(np.stack([x[(i*batch_size+j)*n_samples:(i*batch_size+j+1)*n_samples] for j in range(batch_size)], axis=0)).unsqueeze(1).pin_memory().to(device, non_blocking=True)
+                _, codes = model.encode(batch)
+                codes = codes.permute(0, 2, 1).cpu().detach().numpy()
+                if test:
+                    # will introduce padding but thats fine for encode only
+                    x_, z = model.encode(batch)
+                    y = model.decode(z, x_.shape)
+                    this_samples.append(y.cpu().detach().numpy())
+                this_codes.append(codes)
+            
+            # remainder = len(x) - n_cuts * n_samples
+            i = n_cuts // batch_size
+            remainder = n_cuts - i * batch_size
+            # print(i, remainder, n_cuts, i * batch_size)
+            if remainder > 0:
+                batch = torch.from_numpy(np.stack([x[(i*batch_size+j)*n_samples:(i*batch_size+j+1)*n_samples] for j in range(remainder)], axis=0)).unsqueeze(1).pin_memory().to(device, non_blocking=True)
+                _, codes = model.encode(batch)
+                codes = codes.permute(0, 2, 1).cpu().detach().numpy()
+                if test:
+                    x_, z = model.encode(batch)
+                    y = model.decode(z, x_.shape)
+                    this_samples.append(y.cpu().detach().numpy())
+                this_codes.append(codes)
+
+            # pad with 0s for last section
+            if len(x) - (i * batch_size + remainder) * n_samples > 0:
+                # print(len(x), ' - ', (i * batch_size + remainder) * n_samples, ' == ', len(x) - (i * batch_size + remainder) * n_samples)
+                batch = np.zeros((1, n_samples), dtype=np.float32)
+                batch[0, :len(x) - (i * batch_size + remainder) * n_samples] = x[(i * batch_size + remainder) * n_samples:]
+                batch = torch.from_numpy(batch).unsqueeze(1).pin_memory().to(device, non_blocking=True)
+                _, codes = model.encode(batch)
+                codes = codes.permute(0, 2, 1).cpu().detach().numpy()
+                if test:
+                    x_, z = model.encode(batch)
+                    y = model.decode(z, x_.shape)
+                    this_samples.append(y.cpu().detach().numpy())
+                this_codes.append(codes)
+
+            this_codes = np.concatenate(this_codes, axis=0)
+            binary_instruments = np.zeros(26, dtype=np.uint8)
+            binary_instruments[instruments] = 1
+            binary_instruments = np.tile(binary_instruments, (this_codes.shape[0], this_codes.shape[1], 1))
+
+            all_codes.append(this_codes)
+            all_instruments.append(binary_instruments)
+
+            # for testing
             if test:
-                x_, z = model.encode(batch)
-                y = model.decode(z, x_.shape)
-                this_samples.append(y.cpu().detach().numpy())
-            this_codes.append(codes)
+                this_samples = np.concatenate(this_samples, axis=0).reshape(-1)
+                print(this_codes.shape, x.shape, this_samples.shape)
 
-        # pad with 0s for last section
-        if len(x) - (i * batch_size + remainder) * n_samples > 0:
-            # print(len(x), ' - ', (i * batch_size + remainder) * n_samples, ' == ', len(x) - (i * batch_size + remainder) * n_samples)
-            batch = np.zeros((1, n_samples), dtype=np.float32)
-            batch[0, :len(x) - (i * batch_size + remainder) * n_samples] = x[(i * batch_size + remainder) * n_samples:]
-            batch = torch.from_numpy(batch).unsqueeze(1).pin_memory().to(device, non_blocking=True)
-            _, codes = model.encode(batch)
-            codes = codes.permute(0, 2, 1).cpu().detach().numpy()
-            if test:
-                x_, z = model.encode(batch)
-                y = model.decode(z, x_.shape)
-                this_samples.append(y.cpu().detach().numpy())
-            this_codes.append(codes)
+                sf.write('real.wav', x, rate)
+                sf.write('recon.wav', this_samples, rate)
+            
+            if (idx + 1) % (len(paths) // total_write_batches) == 0:
+                print(f'Writing batch {write_idx}...')
+                all_codes = np.concatenate(all_codes, axis=0)
+                print(all_codes.shape)
 
-        this_codes = np.concatenate(this_codes, axis=0)
-        binary_instruments = np.zeros(26, dtype=np.uint8)
-        binary_instruments[instruments] = 1
-        binary_instruments = np.tile(binary_instruments, (this_codes.shape[0], this_codes.shape[1], 1))
+                all_codes = all_codes.reshape(all_codes.shape[0] * all_codes.shape[1], all_codes.shape[2])
+                print(all_codes.shape)
 
-        all_codes.append(this_codes)
-        all_instruments.append(binary_instruments)
+                filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_{str(write_idx).zfill(2)}.bin')
+                dtype = np.float32
+                arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_codes.shape)
+                arr[:] = all_codes
+                arr.flush()
 
-        # for testing
-        if test:
-            this_samples = np.concatenate(this_samples, axis=0).reshape(-1)
-            print(this_codes.shape, x.shape, this_samples.shape)
+                all_instruments = np.concatenate(all_instruments, axis=0)
+                print(all_instruments.shape)
+                all_instruments = all_instruments.reshape(all_instruments.shape[0] * all_instruments.shape[1], all_instruments.shape[2])
+                print(all_instruments.shape)
 
-            sf.write('real.wav', x, rate)
-            sf.write('recon.wav', this_samples, rate)
-        
-        if (idx + 1) % (len(paths) // total_write_batches) == 0:
-            print(f'Writing batch {write_idx}...')
-            all_codes = np.concatenate(all_codes, axis=0)
-            print(all_codes.shape)
+                filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_instruments_{str(write_idx).zfill(2)}.bin')
+                dtype = np.uint8
+                arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_instruments.shape)
+                arr[:] = all_instruments
+                arr.flush()
 
-            all_codes = all_codes.reshape(all_codes.shape[0] * all_codes.shape[1], all_codes.shape[2])
-            print(all_codes.shape)
+                write_idx += 1
+                write_paths.append((filename, len(all_codes)))
+                all_codes = []
+                all_instruments = []
 
-            filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_{str(write_idx).zfill(2)}.bin')
-            dtype = np.float32
-            arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_codes.shape)
-            arr[:] = all_codes
-            arr.flush()
+    # write the remaining batch
+    print(f'Writing batch {write_idx}...')
+    all_codes = np.concatenate(all_codes, axis=0)
+    all_instruments = np.concatenate(all_instruments, axis=0)
+    print(all_codes.shape, all_instruments.shape)
 
-            all_instruments = np.concatenate(all_instruments, axis=0)
-            print(all_instruments.shape)
-            all_instruments = all_instruments.reshape(all_instruments.shape[0] * all_instruments.shape[1], all_instruments.shape[2])
-            print(all_instruments.shape)
+    all_codes = all_codes.reshape(all_codes.shape[0] * all_codes.shape[1], all_codes.shape[2])
+    all_instruments = all_instruments.reshape(all_instruments.shape[0] * all_instruments.shape[1], all_instruments.shape[2])
+    print(all_codes.shape, all_instruments.shape)
 
-            filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_instruments_{str(write_idx).zfill(2)}.bin')
-            dtype = np.uint8
-            arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_instruments.shape)
-            arr[:] = all_instruments
-            arr.flush()
+    filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_{str(write_idx).zfill(2)}.bin')
+    dtype = np.float32
+    arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_codes.shape)
+    arr[:] = all_codes
+    arr.flush()
 
-            write_idx += 1
-            write_paths.append((filename, len(all_codes)))
-            all_codes = []
-            all_instruments = []
+    filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_instruments_{str(write_idx).zfill(2)}.bin')
+    dtype = np.uint8
+    arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_instruments.shape)
+    arr[:] = all_instruments
+    arr.flush()
 
-# write the remaining batch
-print(f'Writing batch {write_idx}...')
-all_codes = np.concatenate(all_codes, axis=0)
-all_instruments = np.concatenate(all_instruments, axis=0)
-print(all_codes.shape, all_instruments.shape)
-
-all_codes = all_codes.reshape(all_codes.shape[0] * all_codes.shape[1], all_codes.shape[2])
-all_instruments = all_instruments.reshape(all_instruments.shape[0] * all_instruments.shape[1], all_instruments.shape[2])
-print(all_codes.shape, all_instruments.shape)
-
-filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_{str(write_idx).zfill(2)}.bin')
-dtype = np.float32
-arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_codes.shape)
-arr[:] = all_codes
-arr.flush()
-
-filename = os.path.join(os.path.dirname(__file__), f'{out_prefix}_instruments_{str(write_idx).zfill(2)}.bin')
-dtype = np.uint8
-arr = np.memmap(filename, dtype=dtype, mode='w+', shape=all_instruments.shape)
-arr[:] = all_instruments
-arr.flush()
-
-write_idx += 1
-write_paths.append((filename, len(all_codes)))
-all_codes = []
-all_instruments = []
+    write_idx += 1
+    write_paths.append((filename, len(all_codes)))
+    all_codes = []
+    all_instruments = []
 
 ## get token write paths
 dtype = np.float32
@@ -220,7 +222,7 @@ write_paths = []
 paths = [f'{out_prefix}_instruments_{str(i).zfill(2)}.bin' for i in range(49)]
 for path in paths:
     data = np.memmap(path, dtype=dtype, mode='r')
-    data = data.reshape((-1, vae_embed_dim))
+    data = data.reshape((-1, 26))
     write_paths.append((path, len(data)))
 
 # write instruments to instruments_train.bin
