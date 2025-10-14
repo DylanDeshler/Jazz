38503be16f08988819a283f64a29626b6339e70e
Dylan Deshler <dylan.deshler@gmail.com>
2025-10-14 19:05:51 -0400

    low dit

--- neural/train_low_dit.py
+++ neural/train_low_dit.py
@@ -54,7 +54,7 @@ wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
 gradient_accumulation_steps = 2 # used to simulate larger batch sizes
-batch_size = 384# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 256# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 cut_seconds = 1
 cut_len = 32 * cut_seconds
