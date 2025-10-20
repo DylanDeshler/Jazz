fd88e80652e5fff10e04b8d51260fd70db9e4bc0
Dylan Deshler <dylan.deshler@gmail.com>
2025-10-20 15:11:26 -0400

    gamengen

--- neural/train_gamengen.py
+++ neural/train_gamengen.py
@@ -299,7 +299,7 @@ while True:
 
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and master_process:
-        losses = estimate_loss()
+        # losses = estimate_loss()
         if iter_num % sample_interval == 0 and master_process:
             model.eval()
             with ctx:
