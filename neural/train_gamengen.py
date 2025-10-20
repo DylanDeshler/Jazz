028cf1c9649c4abdc3234b788997e8e545af872d
Dylan Deshler <dylan.deshler@gmail.com>
2025-10-20 15:45:38 -0400

    gamengen

--- neural/train_gamengen.py
+++ neural/train_gamengen.py
@@ -245,6 +245,7 @@ def generate_lam_vs_random_actions(step):
 
     n_autoregressive_steps = 5
     alpha = torch.ones(B, device=x.device) * 0.3
+    print(x[:, -n_autoregressive_steps:].shape)
     recon, random_recon = raw_model.lam_vs_random_actions(x[:, -n_autoregressive_steps:].clone(), alpha, n_autoregressive_steps=n_autoregressive_steps, n_diffusion_steps=50, guidance=3)
     
     batches = []
