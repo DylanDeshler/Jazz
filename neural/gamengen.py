c54cc9759afcabc3e6a04d51aa80c5cfe52f9ec3
Dylan Deshler <dylan.deshler@gmail.com>
2025-10-20 23:31:35 -0400

    gamengen

--- neural/gamengen.py
+++ neural/gamengen.py
@@ -669,7 +669,7 @@ class LAM(nn.Module):
         return recon, random
 
 def LAM_M(**kwargs):
-    return LAM(depth=20, hidden_size=1024, num_heads=16, **kwargs)
+    return LAM(depth=20, hidden_size=768, num_heads=12, **kwargs)
 
 def LAM_B(**kwargs):
     return LAM(depth=12, hidden_size=768, num_heads=12, **kwargs)
