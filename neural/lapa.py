608f41645cad9b6954c5d13722fb98faf0ed7839
Dylan Deshler <dylan.deshler@gmail.com>
2025-11-20 02:39:39 -0500

    lapa

--- neural/lapa.py
+++ neural/lapa.py
@@ -515,10 +515,10 @@ class DiTWrapper(nn.Module):
         self.net.initialize_weights()
     
     def forward(self, x, actions, t=None):
-        return self.diffusion.target_loss(self.net, x, x[:, 1], t=t, net_kwargs={'actions': actions})
+        return self.diffusion.target_loss(self.net, x[:, 1], x[:, 1], t=t, net_kwargs={'actions': actions})
     
     def sample(self, x, actions, n_steps=50):
-        return self.sampler.sample(self.net, x.shape, n_steps, net_kwargs={'x': x, 'actions': actions})
+        return self.sampler.sample(self.net, x[:, 1].shape, n_steps, net_kwargs={'x': x, 'actions': actions})
 
 class LAM(nn.Module):
     def __init__(self,
