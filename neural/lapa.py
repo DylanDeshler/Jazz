900747a2f8656d51401be29607a4a650e01a7013
Dylan Deshler <dylan.deshler@gmail.com>
2025-11-21 00:23:04 -0500

    lapa

--- neural/lapa.py
+++ neural/lapa.py
@@ -364,8 +364,8 @@ class ActionTransformer(nn.Module):
         ])
         
         self.to_vq = nn.Sequential(
-            nn.LayerNorm(hidden_size),
-            nn.Linear(hidden_size, len(levels)),
+            nn.LayerNorm(spatial_window * hidden_size),
+            nn.Linear(spatial_window * hidden_size, len(levels)),
         )
         self.vq = FSQ(levels=levels)
         self.from_vq = nn.Linear(len(levels), hidden_size)
