0ae22bd3b431e8e2ae5dae81b0df6e96e8d18643
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-01 18:29:02 -0500

    beats

--- neural/train_low_beats_dito6.py
+++ neural/train_low_beats_dito6.py
@@ -54,7 +54,7 @@ wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
 gradient_accumulation_steps = 2 # used to simulate larger batch sizes
-batch_size = 32 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 24 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 rate = 16000
 n_samples = 2**16
