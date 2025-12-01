f0fed0a9822c9d8f2d0a9099e3bc6091d5b1baa9
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-01 13:01:39 -0500

    beats

--- preprocess/beat_this.py
+++ preprocess/beat_this.py
@@ -1,3 +1,7 @@
+"""
+Requires installing beat_this from https://github.com/CPJKU/beat_this
+"""
+
 import os
 import glob
 from tqdm import tqdm
@@ -11,5 +15,5 @@ file2beats = File2Beats(checkpoint_path="final0", device="cuda", dbn=False)
 for audio_path in tqdm(paths):
     beats, downbeats = file2beats(audio_path)
 
-    outpath = audio_path.replace('jazz_data_16000_full_clean', 'jazz_data_16000_full_clean_beats')
+    outpath = audio_path.replace('jazz_data_16000_full_clean', 'jazz_data_16000_full_clean_beats').replace('.wav', '.beats')
     save_beat_tsv(beats, downbeats, outpath)
