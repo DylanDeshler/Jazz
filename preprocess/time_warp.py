f32211d01a88254a2579d7c0f511a68f640dc457
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-04 12:43:45 -0500

    beats

--- preprocess/time_warp.py
+++ preprocess/time_warp.py
@@ -234,7 +234,6 @@ def crunch():
     length = 0
     for path in tqdm(paths, desc='Calculating Total Length'):
         length += len(np.load(path)['audio'])
-        print(length)
     
     audio_mmap = np.memmap(
         '/home/dylan.d/research/music/Jazz/jazz_data_16000_full_clean_measures_audio.npy', 
