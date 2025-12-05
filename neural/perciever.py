d38e0c2835e4c32834bfdee78f67128ad9a00f22
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-05 01:38:10 -0500

    perciever

--- neural/perciever.py
+++ neural/perciever.py
@@ -485,7 +485,7 @@ if __name__ == '__main__':
         encoder = Perciever(1, 512, 16, 8, 4, 4, 32).to('cuda:1')
         summary(encoder)
         
-        decoder = Reciever(1, 512, 16, 8, 8, 3, 32, 7).to('cuda:1')
+        decoder = Reciever(1, 512, 16, 8, 8, 3, 32, None, 7).to('cuda:1')
         summary(decoder)
         
         x = torch.randn((64, 1, 16000)).to('cuda:1')
