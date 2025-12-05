ab3836f9dadf2af8f3735823910301092fa635c5
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-05 01:52:43 -0500

    perciever

--- neural/perciever.py
+++ neural/perciever.py
@@ -359,7 +359,7 @@ class CrossAttentionBlock(nn.Module):
         self.mlp = SwiGLUMlp(hidden_size, int(2 / 3 * mlp_ratio * hidden_size))
     
     def forward(self, x, context, freqs_cis=None, attn_mask=None):
-        x = x + self.attn(self.norm1(x), context)
+        x = x + self.attn(self.norm1(x), context, attn_mask=attn_mask)
         x = x + self.mlp(self.norm2(x))
         return x
 
@@ -375,8 +375,7 @@ class SelfAttentionBlock(nn.Module):
         self.mlp = SwiGLUMlp(hidden_size, int(2 / 3 * mlp_ratio * hidden_size))
     
     def forward(self, x, context=None, freqs_cis=None, attn_mask=None):
-        print(x.shape, freqs_cis.shape)
-        x = x + self.attn(self.norm1(x), freqs_cis=freqs_cis)
+        x = x + self.attn(self.norm1(x), freqs_cis=freqs_cis, attn_mask=attn_mask)
         x = x + self.mlp(self.norm2(x))
         return x
 
