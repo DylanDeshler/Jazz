72b5e2589a5c338ab0c07ff06e8d061f0d5b9afd
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-05 00:40:32 -0500

    perciever

--- neural/perciever.py
+++ neural/perciever.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.attention.flex_attention import flex_attention, create_block_mask
 
 import math
 from typing import Optional, List, Callable
@@ -116,6 +117,70 @@ class Attention(nn.Module):
                 is_causal=is_causal,
                 dropout_p=self.attn_drop.p if self.training else 0.,
             )
+        else:
+            raise NotImplementedError()
+            q = q * self.scale
+            attn = q @ k.transpose(-2, -1)
+            attn = maybe_add_mask(attn, attn_mask)
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            x = attn @ v
+
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class WindowAttention(nn.Module):
+    def __init__(
+            self,
+            dim: int,
+            window_size: int,
+            num_heads: int = 8,
+            qkv_bias: bool = False,
+            proj_bias: bool = True,
+            attn_drop: float = 0.,
+            proj_drop: float = 0.,
+    ) -> None:
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+        self.fused_attn = True
+        self.window_size = window_size
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim, bias=proj_bias)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(
+            self,
+            x: torch.Tensor,
+            freqs_cis: Optional[torch.Tensor] = None,
+            attn_mask = None,
+    ) -> torch.Tensor:
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+        
+        # RoPE
+        if freqs_cis is not None:
+            q = apply_rotary_emb(q.transpose(1, 2), freqs_cis).transpose(1, 2)
+            k = apply_rotary_emb(k.transpose(1, 2), freqs_cis).transpose(1, 2)
+
+        if self.fused_attn:
+            print(q.shape, k.shape)
+            block_mask = create_block_mask(
+                generate_window_mask_mod(self.window_size), 
+                B=None, H=None, Q_LEN=q.shape[0], KV_LEN=k.shape[0], 
+                device=q.device
+            )
+            print(block_mask.shape, block_mask.dtype)
+            block_mask = block_mask & attn_mask
+            
+            x = flex_attention(q, k, v, block_mask=block_mask)
         else:
             raise NotImplementedError()
             q = q * self.scale
@@ -251,6 +316,16 @@ class SelfAttentionBlock(nn.Module):
         x = x + self.mlp(self.norm2(x))
         return x
 
+def generate_window_mask_mod(window_size):
+    """
+    Returns a score_mod function for flex_attention that enforces
+    local window constraints.
+    """
+    def window_mask(b, h, q_idx, kv_idx):
+        # Only attend if Query and Key are in the same window block
+        return (q_idx // window_size) == (kv_idx // window_size)
+    return window_mask
+
 class ContinuousPositionalEmbeddings(nn.Module):
     def __init__(self, dim, max_freq=10):
         super().__init__()
@@ -311,16 +386,58 @@ class Perciever(nn.Module):
         x = self.out_proj(x)
         return x
 
+class Reciever(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim, n_heads, depth, n_interleave, n_latents, window_size):
+        super().__init__()
+        self.n_latents = n_latents
+        
+        self.in_proj = nn.Linear(in_dim, hidden_dim)
+        self.pos_emb = ContinuousPositionalEmbeddings(hidden_dim)
+        
+        layers = []
+        for d in range(depth):
+            layers.append(CrossAttentionBlock(hidden_dim, n_heads))
+            for _ in range(n_interleave):
+                layers.append(WindowAttention(hidden_dim, window_size, n_heads))
+        
+        self.layers = nn.ModuleList(layers)
+        
+        self.norm = RMSNorm(hidden_dim)
+        self.out_proj = nn.Linear(hidden_dim, out_dim)
+    
+    def forward(self, x, z):
+        B, L, C = x.shape
+        
+        x = self.in_proj(x)
+        x = x + self.pos_emb(torch.linspace(0, 1, steps=L, device=x.device).unsqueeze(0))
+        
+        z = z + self.pos_emb(torch.linspace(0, 1, steps=z.shape[1], device=z.device).unsqueeze(0))
+        
+        for layer in self.layers:
+            x = layer(x, z)
+        
+        x = self.norm(x)
+        x = self.out_proj(x)
+        return x
+
 if __name__ == '__main__':
     from torchinfo import summary
     
-    model = Perciever(1, 512, 16, 8, 4, 4, 32).to('cuda:1')
-    summary(model)
-    
-    x = torch.randn((64, 1, 16000)).to('cuda:1')
-    y = model(x)
-    print(x.shape, y.shape)
-    
-    x = torch.randn((32, 1, 48000)).to('cuda:1')
-    y = model(x)
-    print(x.shape, y.shape)
+    with torch.no_grad():
+        encoder = Perciever(1, 512, 16, 8, 4, 4, 32).to('cuda:1')
+        summary(encoder)
+        
+        decoder = Reciever(16, 512, 1, 8, 4, 4, 32, 256).to('cuda:1')
+        summary(decoder)
+        
+        x = torch.randn((64, 1, 16000)).to('cuda:1')
+        y = encoder(x)
+        print(x.shape, y.shape)
+        z = decoder(x, y)
+        print(z.shape)
+        
+        x = torch.randn((32, 1, 48000)).to('cuda:1')
+        y = encoder(x)
+        print(x.shape, y.shape)
+        z = decoder(x, y)
+        print(z.shape)
