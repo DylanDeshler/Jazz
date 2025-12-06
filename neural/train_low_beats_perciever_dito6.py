2738f07f38c2bf7a6632c413726b59162112ed41
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-06 04:40:36 -0500

    pers

--- neural/train_low_beats_perciever_dito6.py
+++ neural/train_low_beats_perciever_dito6.py
@@ -55,7 +55,7 @@ wandb_project = out_dir #'zinc20++'
 wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 4 # used to simulate larger batch sizes
+gradient_accumulation_steps = 8 # used to simulate larger batch sizes
 batch_size = 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 rate = 16000
