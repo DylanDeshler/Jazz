8fb2b70e6d2257b425bd21889193aeb5be2a0b55
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-07 19:14:52 -0500

    beats

--- neural/generate_continuous_measures_dataset.py
+++ neural/generate_continuous_measures_dataset.py
@@ -12,11 +12,6 @@ import torch
 
 from dito import DiToV4 as Tokenizer
 
-import matplotlib.pyplot as plt
-import soundfile as sf
-import librosa
-import glob
-
 device = torch.device('cuda')
 
 batch_size = 128
@@ -44,10 +39,11 @@ N = 3693787
 data = np.memmap('/home/dylan.d/research/music/Jazz/jazz_data_16000_full_clean_measures_audio.npy', dtype=np.float16, mode='r', shape=(N, n_samples))
 arr = np.memmap('/home/dylan.d/research/music/Jazz/latents/low_measures_large.bin', dtype=np.float16, mode='w+', shape=(N, 48))
 
-for i in range(N // batch_size):
-    batch = torch.from_numpy(data[i*batch_size:(i+1)*batch_size]).view(batch_size, n_samples).unsqueeze(1).pin_memory().to(device, non_blocking=True)
-    _, codes = model.encode(batch)
-    codes = codes.permute(0, 2, 1).cpu().detach().numpy()
-    arr[i*batch_size:(i+1)*batch_size] = codes.astype(np.float16)
+with torch.no_grad():
+    for i in tqdm(range(N // batch_size)):
+        batch = torch.from_numpy(data[i*batch_size:(i+1)*batch_size].copy()).view(batch_size, n_samples).unsqueeze(1).pin_memory().to(device, non_blocking=True)
+        _, codes = model.encode(batch)
+        codes = codes.permute(0, 2, 1).cpu().detach().numpy()
+        arr[i*batch_size:(i+1)*batch_size] = codes.astype(np.float16)
 
 arr.flush()
