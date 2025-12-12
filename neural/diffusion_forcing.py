eaeb54e2727f35a7143a2208165b013d606eb6c3
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-12 04:22:45 -0500

    dit

--- neural/diffusion_forcing.py
+++ neural/diffusion_forcing.py
@@ -497,6 +497,7 @@ class DiT(nn.Module):
         print(t.shape, bpm.shape, actions.shape)
         context = torch.cat([t.unsqueeze(-2), bpm.unsqueeze(-2), actions], dim=-2).view(t.shape[0], self.n_chunks * (2 + self.action_length), t.shape[-1]).contiguous()
         
+        print(x.shape, context.shape)
         x = x + self.x_pos(torch.arange(x.shape[1], device=x.device, dtype=torch.long).unsqueeze(0))
         context = context + self.context_pos(torch.arange(t.shape[1] * (2 + self.num_actions), device=x.device, dtype=torch.long).unsqueeze(0))
         for block in self.blocks:
