e0e103a0ad84ffbf2c87725022ff38d039e31c2a
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-12 13:42:37 -0500

    dit

--- neural/diffusion_forcing.py
+++ neural/diffusion_forcing.py
@@ -465,6 +465,7 @@ def token_drop(labels, null_token, dropout_prob, force_drop_ids=None):
     shape = [1] * (labels.ndim - 1) + [labels.shape[-1]]
     print(null_token.view(shape).shape)
     # labels = torch.where(drop_ids, null_token.view(shape), labels)
+    print(labels.dtype, drop_ids.dtype, null_token.dtype)
     labels[drop_ids] = null_token.view(shape)
     return labels
 
