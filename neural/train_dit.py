5ed888df16dfff9d376d73a1ec7e57218bf8cdd9
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-12 14:35:32 -0500

    dit

--- neural/train_dit.py
+++ neural/train_dit.py
@@ -60,11 +60,11 @@ wandb_project = out_dir #'zinc20++'
 wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 2 # used to simulate larger batch sizes
-batch_size = 128# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
+gradient_accumulation_steps = 3 # used to simulate larger batch sizes
+batch_size = 64# * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 spatial_window = 48
-n_chunks = 5
+n_chunks = 10
 max_seq_len = spatial_window * n_chunks
 action_length = 3
 vae_embed_dim = 16
