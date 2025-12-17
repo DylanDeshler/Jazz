a77e64b34a60333bc64bc489f753efd3a7e66663
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-17 09:12:40 -0500

    dit

--- neural/train_dit.py
+++ neural/train_dit.py
@@ -73,14 +73,14 @@ vae_embed_dim = 16
 levels = [5, 3]
 # adamw optimizer
 learning_rate = 1e-4 # max learning rate
-max_iters = 1000000 # total number of training iterations
+max_iters = 115000 # total number of training iterations
 weight_decay = 1e-2
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
-decay_lr = False # whether to decay the learning rate
-warmup_iters = 5000 # how many steps to warm up for
+decay_lr = True # whether to decay the learning rate
+warmup_iters = 95000 # how many steps to warm up for
 lr_decay_iters = max_iters # should be ~= max_iters per Chinchilla
 min_lr = learning_rate / 10 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
