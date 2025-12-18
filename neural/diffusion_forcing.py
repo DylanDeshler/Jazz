3b8721e324f1b2c246d13f8aa719e32b2b747ac6
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-18 06:05:00 -0500

    dit

--- neural/diffusion_forcing.py
+++ neural/diffusion_forcing.py
@@ -723,6 +723,7 @@ class ModernDiT(nn.Module):
         shift, scale = (self.final_layer_scale_shift_table[None, None] + F.silu(t[:, :, None])).chunk(
             2, dim=1
         )
+        print(x.shape, shift.shape, scale.shape)
         x = modulate(self.norm(x), shift, scale)
         x = self.fc(x)
         return x
