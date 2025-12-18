96ac253f0f95dddda7633a70f1349fe8173113d6
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-18 07:15:34 -0500

    dit

--- neural/train_modern_dit.py
+++ neural/train_modern_dit.py
@@ -55,7 +55,7 @@ wandb_project = out_dir #'zinc20++'
 wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 1 # used to simulate larger batch sizes
+gradient_accumulation_steps = 2 # used to simulate larger batch sizes
 batch_size = 96 # * 5 * 8 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 spatial_window = 48
