248db809ba789d08d98462b289cb6a7004bcb235
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-19 12:54:11 -0500

    lapa

--- neural/train_modern_lapa_measures.py
+++ neural/train_modern_lapa_measures.py
@@ -411,7 +411,7 @@ while True:
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and master_process:
         # usage = estimate_codebook_usage()
-        losses = estimate_loss()
+        # losses = estimate_loss()
         if iter_num % sample_interval == 0 and master_process:
             model.eval()
             with ctx:
