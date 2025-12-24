e2334ebcc870969ec04cf74efa272cddc52f3871
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-24 04:53:34 -0500

    beats

--- preprocess/annotate_beats.py
+++ preprocess/annotate_beats.py
@@ -13,7 +13,14 @@ paths = glob.glob('/home/ubuntu/base/Data/wavs/*.wav')
 file2beats = File2Beats(checkpoint_path="final0", device="cuda", dbn=False)
 
 for audio_path in tqdm(paths):
-    beats, downbeats = file2beats(audio_path)
-
     outpath = audio_path.replace('wavs', 'beats').replace('.wav', '.beats')
-    save_beat_tsv(beats, downbeats, outpath)
+    
+    if os.path.exists(outpath):
+        continue
+    
+    try:
+        beats, downbeats = file2beats(audio_path)
+        save_beat_tsv(beats, downbeats, outpath)
+    except Exception as e:
+        print(e)
+        continue
