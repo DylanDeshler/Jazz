48f16d7faa7206bee423f706db683b75dbeec821
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-26 20:44:51 -0500

    idm

--- neural/idm.py
+++ neural/idm.py
@@ -560,8 +560,7 @@ class DiTBlock(nn.Module):
         )
     
     def forward(self, x, t, freqs_cis=None, attn_mask=False):
-        print(x.shape, t.shape)
-        biases = self.scale_shift_table[None] + t.reshape(x.size(0), 6, -1)
+        biases = self.scale_shift_table[None] + t.reshape(x.size(0), x.size(1), 6, -1)
         (
             shift_msa,
             scale_msa,
@@ -569,7 +568,7 @@ class DiTBlock(nn.Module):
             shift_mlp,
             scale_mlp,
             gate_mlp,
-        ) = [chunk for chunk in biases.chunk(6, dim=1)]
+        ) = [chunk.squeeze(2) for chunk in biases.chunk(6, dim=2)]
         
         x = x + gate_msa * self.attn(modulate(self.norm1(x), shift_msa, scale_msa), freqs_cis=freqs_cis, attn_mask=attn_mask)
         x = x + gate_mlp * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))
