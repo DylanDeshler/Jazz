de4381d4140645a35f24320abc783ede9362b50e
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-26 20:28:05 -0500

    idm

--- neural/idm.py
+++ neural/idm.py
@@ -527,7 +527,8 @@ class ActionTransformer(nn.Module):
         x = self.x_embedder(x)
         bpm = self.bpm_embedder(bpm)
         
-        x = torch.cat([bpm, x], dim=2)
+        print(x.shape, bpm.shape)
+        x = torch.cat([bpm, x], dim=-1)
         x = rearrange(x, 'b t n c -> (b t) n c')
         x = x + self.spatial_pos(torch.arange(N+1, device=x.device, dtype=torch.long).unsqueeze(0))
         for block in self.spatial_blocks:
