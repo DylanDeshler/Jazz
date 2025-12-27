d96de0ab1934526c38df87e108250c10be413125
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-26 20:22:08 -0500

    idm

--- neural/train_idm.py
+++ neural/train_idm.py
@@ -376,7 +376,7 @@ if wandb_log and master_process:
     wandb.init(project=wandb_project, name=wandb_run_name, config=config)
 
 # training loop
-X, ratio, bpm, actions = get_batch('train') # fetch the very first batch
+X, ratio, bpm = get_batch('train') # fetch the very first batch
 t0 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
 raw_model = model.module if ddp else model # unwrap DDP container if needed
@@ -447,7 +447,7 @@ while True:
             loss, _ = model(X, bpm)
             loss = loss / gradient_accumulation_steps # scale the loss to account for gradient accumulation
         # immediately async prefetch next batch while model is doing the forward pass on the GPU
-        X, ratio, bpm, actions = get_batch('train')
+        X, ratio, bpm = get_batch('train')
         # backward pass, with gradient scaling if training in fp16
         scaler.scale(loss).backward()
     # clip the gradient
