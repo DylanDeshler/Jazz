1b558853e4bc892782e41ab73b95736aee3c8162
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-26 20:27:16 -0500

    idm

--- neural/idm.py
+++ neural/idm.py
@@ -223,10 +223,10 @@ class TimestepEmbedder(nn.Module):
             self.mlp = SwiGLUMlp(frequency_embedding_size, int(2 / 3 * 4 * hidden_size), hidden_size, bias=bias)
         else:
             self.mlp = nn.Sequential(
-            nn.Linear(frequency_embedding_size, hidden_size, bias=bias),
-            nn.SiLU(),
-            nn.Linear(hidden_size, hidden_size, bias=bias),
-        )
+                nn.Linear(frequency_embedding_size, hidden_size, bias=bias),
+                nn.SiLU(),
+                nn.Linear(hidden_size, hidden_size, bias=bias),
+            )
         self.frequency_embedding_size = frequency_embedding_size
         self.max_period = max_period
 
@@ -234,18 +234,18 @@ class TimestepEmbedder(nn.Module):
     def timestep_embedding(t, dim, max_period=10000):
         """
         Create sinusoidal timestep embeddings.
-        :param t: a 1-D Tensor of (N) indices, one per batch element.
+        :param t: a 2-D Tensor of (N, T) indices, one per batch element.
                           These may be fractional.
         :param dim: the dimension of the output.
         :param max_period: controls the minimum frequency of the embeddings.
-        :return: an (N, D) Tensor of positional embeddings.
+        :return: an (N, T, D) Tensor of positional embeddings.
         """
         # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
         half = dim // 2
         freqs = torch.exp(
             -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
         ).to(device=t.device)
-        args = t[:, None].float() * freqs[None]
+        args = t[:, :, None].float() * freqs[None]
         embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
         if dim % 2:
             embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
