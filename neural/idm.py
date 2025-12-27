153f1f84958664f8ecd9c35de915d4924dc089b2
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-27 05:11:48 -0500

    idm

--- neural/idm.py
+++ neural/idm.py
@@ -674,7 +674,6 @@ class ModernDiT(nn.Module):
     def forward(self, x, t, bpm, actions, clean_x):
         # drop 1st token because no action for it
         t = t[:, 1:]
-        x = x[:, 1:]
         
         bpm = self.bpm_embedder(bpm)
         bpm = torch.cat([bpm[:, 1:], bpm[:, :-1]], dim=-1)
