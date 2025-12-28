cb972a7b2ee19dcdd7dac911f11fea9225944f89
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-28 17:17:22 -0500

    idm

--- neural/train_idm.py
+++ neural/train_idm.py
@@ -50,7 +50,7 @@ eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = True # disabled by default
+wandb_log = False # disabled by default
 wandb_project = out_dir
 wandb_run_name = str(time.time())
 # data
