8b1684c16c918e1f639949f6fa3bc2254944009d
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-29 16:55:06 -0500

    idm

--- neural/raw_idm.py
+++ neural/raw_idm.py
@@ -702,8 +702,11 @@ class ModernDiT(nn.Module):
             x = block(x, t0, freqs_cis=freqs_cis, attn_mask=self.block_causal_mask)
         
         # SAM Audio does not use a non-linearity on t here
-        shift, scale = (self.final_layer_scale_shift_table[None] + F.silu(t[:, None])).chunk(
-            2, dim=1
+        # shift, scale = (self.final_layer_scale_shift_table[None] + F.silu(t[:, None])).chunk(
+        #     2, dim=1
+        # )
+        shift, scale = (self.final_layer_scale_shift_table[None, None] + F.silu(t[:, :, None])).chunk(
+            3, dim=1
         )
         x = modulate(self.norm(x), shift, scale)
         x = self.fc(x)
