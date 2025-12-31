c4fd5e76059ce52c1263ec49baa40de4ff25fe2a
Dylan Deshler <dylan.deshler@gmail.com>
2025-12-31 04:05:22 -0500

    style

--- neural/train_style.py
+++ neural/train_style.py
@@ -50,11 +50,11 @@ save_interval = 5000
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
 wandb_project = out_dir
-wandb_run_name = '1767155853.2994754'#str(time.time())
+wandb_run_name = str(time.time())
 # data
 dataset = ''
 gradient_accumulation_steps = 1 # used to simulate larger batch sizes
