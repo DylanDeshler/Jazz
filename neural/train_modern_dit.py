b9835b34470e64985738476a6c720335f2666bbf
Dylan Deshler <dylan.deshler@gmail.com>
2026-01-09 04:05:28 -0500

    dit

--- neural/train_modern_dit.py
+++ neural/train_modern_dit.py
@@ -74,8 +74,8 @@ beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 5000 # how many steps to warm up for
-lr_decay_iters = 105000 # should be ~= max_iters per Chinchilla
+warmup_iters = 110000 # how many steps to warm up for
+lr_decay_iters = max_iters # should be ~= max_iters per Chinchilla
 min_lr = learning_rate / 10 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
