b0b5336c4c274405c1995602d2ca51c4678853c7
Dylan Deshler <dylan.deshler@gmail.com>
2026-01-30 10:19:30 -0500

    style

--- neural/train_style.py
+++ neural/train_style.py
@@ -42,16 +42,17 @@ import matplotlib.pyplot as plt
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'Style_256_adaln_1measures_bpm_S_nobias_poolfirst_norm_nohistory_1head_top5'
-eval_interval = 5000
+out_dir = 'test'
+eval_interval = 500
 sample_interval = 5000
 log_interval = 100
 save_interval = 5000
 eval_iters = 400
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = True # disabled by default
+wandb_log = False # disabled by default
 wandb_project = out_dir
 wandb_run_name = str(time.time())
 # data
@@ -69,14 +70,14 @@ vae_embed_dim = 16
 n_style_embeddings = 256
 # adamw optimizer
 learning_rate = 1e-4 # max learning rate
-max_iters = 170000 # total number of training iterations
+max_iters = 5000 # total number of training iterations
 weight_decay = 1e-2
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
-decay_lr = True # whether to decay the learning rate
-warmup_iters = 145000 # how many steps to warm up for
+decay_lr = False # whether to decay the learning rate
+warmup_iters = 1000000 # how many steps to warm up for
 lr_decay_iters = max_iters # should be ~= max_iters per Chinchilla
 min_lr = learning_rate / 10 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
