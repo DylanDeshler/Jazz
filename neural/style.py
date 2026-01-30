29ba380247e1958e0136cca16740e5379d35d3c0
Dylan Deshler <dylan.deshler@gmail.com>
2026-01-30 10:53:39 -0500

    style

--- neural/style.py
+++ neural/style.py
@@ -615,12 +615,12 @@ class ActionTransformer(nn.Module):
         style_embeddings = self.pool_norm(self.style_embeddings.unsqueeze(0).repeat(B, 1, 1))
         
         # loses x signal but interpretable
-        query = torch.mean(x, dim=-2, keepdim=False)
-        style, weights = self.pool_attn(query=query, key=style_embeddings, value=style_embeddings, return_weights=True)
+        # query = torch.mean(x, dim=-2, keepdim=False)
+        # style, weights = self.pool_attn(query=query, key=style_embeddings, value=style_embeddings, return_weights=True)
         
         # better but less interpretable?
-        # style = self.pool_attn(query=x, key=style_embeddings, value=style_embeddings)
-        # style = torch.mean(style, dim=-2, keepdim=False)
+        style = self.pool_attn(query=x, key=style_embeddings, value=style_embeddings)
+        style = torch.mean(style, dim=-2, keepdim=False)
         
         weights = weights.squeeze(-2)
         weights = weights.mean(dim=1)
