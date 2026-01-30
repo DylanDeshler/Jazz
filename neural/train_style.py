1f25d155d41c2a9dfde52ea5271e7ec21f3c2bb0
Dylan Deshler <dylan.deshler@gmail.com>
2026-01-30 12:19:57 -0500

    style

--- neural/train_style.py
+++ neural/train_style.py
@@ -48,10 +48,10 @@ log_interval = 100
 save_interval = 5000
 eval_iters = 400
 eval_only = False # if True, script exits right after the first eval
-always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+always_save_checkpoint = True # if True, always save a checkpoint after each eval
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = True # disabled by default
+wandb_log = False # disabled by default
 wandb_project = out_dir
 wandb_run_name = str(time.time())
 # data
