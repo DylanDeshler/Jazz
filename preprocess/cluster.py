d63d13634c0938bd87e58dec5ce157510b3afc72
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-02 16:04:53 -0500

    cluster

--- preprocess/cluster.py
+++ preprocess/cluster.py
@@ -6,7 +6,7 @@ import numpy as np
 spatial_window = 48
 vae_embed_dim = 16
 N = 4403211
-X = np.memmap('/home/ubuntu/Data/low_measures_large.bin', dtype=np.float16, mode='r', shape=(N, spatial_window, vae_embed_dim)).reshape(N, -1)
+X = np.memmap('/home/ubuntu/Data/low_measures_large.bin', dtype=np.float16, mode='r', shape=(N, spatial_window, vae_embed_dim)).copy().reshape(N, -1)
 X = normalize(X, norm='l2', axis=1)
 
 kmeans = KMeans(n_clusters=256, n_init=100, max_iter=1000, verbose=2)
