87f980d47cfbdc36545d870771b18736fd480313
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-02 16:37:00 -0500

    cluster

--- preprocess/cluster.py
+++ preprocess/cluster.py
@@ -8,18 +8,22 @@ vae_embed_dim = 16
 N = 4403211
 
 print('Loading data...')
-X = np.memmap('/home/ubuntu/Data/low_measures_large.bin', dtype=np.float16, mode='r', shape=(N, spatial_window, vae_embed_dim)).copy().reshape(N, -1)
+X = np.memmap('/home/ubuntu/Data/low_measures_large.bin', dtype=np.float16, mode='r', shape=(N, spatial_window, vae_embed_dim)).copy()
+# X = X.reshape(N, -1)
+X = X.mean(1)
+# X = np.concatenate([X.mean(1), X.std(1)], -1)
 
 print('Normalizing...')
-X = normalize(X, norm='l2', axis=1)
+X = normalize(X, norm='l2', axis=-1)
 
 print('Fitting')
-kmeans = KMeans(n_clusters=256, n_init=100, max_iter=1000, verbose=2, n_jobs=-1)
+np.random.seed(0)
+kmeans = KMeans(n_clusters=256, n_init=100, max_iter=1000, verbose=2, random_state=0)
 kmeans.fit(X)
 
 print('Renormalizing...')
 centroids = kmeans.cluster_centers_
-style_bank = normalize(centroids, norm='l2', axis=1)
+style_bank = normalize(centroids, norm='l2', axis=-1)
 
 print('Writing...')
 with open('/home/ubuntu/Data/low_measures_large_clusters.npy', 'w+') as f:
