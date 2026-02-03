486ba579c7dd5c694ebd3b734cb019e9dbfecccb
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-02 20:01:50 -0500

    style

--- neural/style.py
+++ neural/style.py
@@ -819,7 +819,7 @@ class ActionTransformer(nn.Module):
         # better but less interpretable?
         style, weights = self.pool_attn(query=x, key=style_embeddings, value=style_embeddings, return_weights=True)
         style = torch.mean(style, dim=-2, keepdim=False)
-        
+        print(weights.shape)
         weights = weights.squeeze(-2)
         weights = weights.mean(dim=1)
         
