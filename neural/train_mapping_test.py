f50b49da1679525af476299db638247263e6f9f5
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-04 18:30:07 -0500

    map

--- neural/train_mapping_test.py
+++ neural/train_mapping_test.py
@@ -30,7 +30,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 from einops import rearrange
 
-from diffusion_forcing import ModernDiT_large as net
+from diffusion_forcing import ModernDiT_small as net
 from dito import DiToV5 as Tokenizer
 import soundfile as sf
 
