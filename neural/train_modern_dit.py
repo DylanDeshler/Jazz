78385ac40448f4c65fa58a7129be147d92b08689
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-04 20:27:18 -0500

    dit

--- neural/train_modern_dit.py
+++ neural/train_modern_dit.py
@@ -59,7 +59,7 @@ gradient_accumulation_steps = 2
 batch_size = 80#384
 # model
 spatial_window = 48
-n_chunks = 20
+n_chunks = 15
 max_seq_len = spatial_window * n_chunks
 vae_embed_dim = 16
 n_style_embeddings = 256
