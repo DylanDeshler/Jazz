6e3404d12478d417290c97c1acd30296873637ae
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-09 15:08:13 -0500

    map

--- neural/train_mapping.py
+++ neural/train_mapping.py
@@ -35,7 +35,7 @@ from mapping import MLP_B as net
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
-out_dir = 'style_mapping_256top5_64_copy'
+out_dir = 'style_mapping_256top5_64_redo'
 eval_interval = 5000
 sample_interval = 5000
 log_interval = 100
@@ -43,7 +43,7 @@ save_interval = 5000
 eval_iters = 400
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = False # disabled by default
 wandb_project = out_dir
