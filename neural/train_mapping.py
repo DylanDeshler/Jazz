b761428192db354f487c30f4d5f77598ae42861a
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-09 15:02:59 -0500

    map

--- neural/train_mapping.py
+++ neural/train_mapping.py
@@ -30,7 +30,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 from einops import rearrange
 
-from mapping import DiffusionMLP_B as net
+from mapping import MLP_B as net
 import soundfile as sf
 from scipy import signal
 
@@ -40,7 +40,7 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
-out_dir = 'style_diffusion_mapping_256top5_64'
+out_dir = 'style_mapping_256top5_64_copy'
 eval_interval = 5000
 sample_interval = 5000
 log_interval = 100
@@ -48,7 +48,7 @@ save_interval = 5000
 eval_iters = 400
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = False # disabled by default
 wandb_project = out_dir
