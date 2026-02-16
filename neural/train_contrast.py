da699e3e10f80e547b098903eb2721f96d435e29
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-16 17:41:18 -0500

    contrast

--- neural/train_contrast.py
+++ neural/train_contrast.py
@@ -129,9 +129,10 @@ with open('/home/dylan.d/research/music/Jazz/file_offsets.pkl', 'rb') as f:
     file_offsets = np.array([x[1:] for x in file_offsets], dtype=np.int64)
 
 def sample_non_overlapping(data, start_fraction, end_fraction):
-    pos = np.random.choice(np.arange(int(len(file_offsets) * start_fraction), int(len(file_offsets) * end_fraction)), size=(batch_size // 2, ), replace=False)
-    starts = torch.from_numpy(file_offsets[pos, 0])#.repeat_interleave(2, dim=0)
-    lengths = torch.from_numpy(file_offsets[pos, 1])#.repeat_interleave(2, dim=0)
+    # pos = np.random.choice(np.arange(int(len(file_offsets) * start_fraction), int(len(file_offsets) * end_fraction)), size=(batch_size // 2, ), replace=False)
+    pos = np.random.randint(int(len(file_offsets) * start_fraction), int(len(file_offsets) * end_fraction), size=(batch_size // 2,))
+    starts = torch.from_numpy(file_offsets[pos, 0]).repeat_interleave(2, dim=0)
+    lengths = torch.from_numpy(file_offsets[pos, 1]).repeat_interleave(2, dim=0)
     idxs = starts + torch.rand(lengths.shape) * lengths
     # idxs = torch.cat([torch.randint(start, start + length - n_samples, size=(2,)) for start, length in zip(starts, lengths)], dim=0)
     return idxs.long()
