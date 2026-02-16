db6b0547f5da6b944d88603dbb992057b8baa1d4
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-16 17:12:34 -0500

    contrast

--- neural/contrast.py
+++ neural/contrast.py
@@ -434,7 +434,7 @@ class Transformer(nn.Module):
         
         x = self.pool(x.mean(1, keepdims=True), x).squeeze(1)
         x = self.fc(x)
-        loss = self._compute_loss(x)
+        loss = 0#self._compute_loss(x)
         
         out = {'loss': loss, 'z': x}
         
