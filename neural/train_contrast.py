16581b8d866d3a233683afb3dd3fb442dbd8a822
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-16 18:02:30 -0500

    contrast

--- neural/train_contrast.py
+++ neural/train_contrast.py
@@ -133,13 +133,13 @@ def sample_non_overlapping(start_fraction, end_fraction):
     pos = np.random.choice(np.arange(int(len(file_offsets) * start_fraction), int(len(file_offsets) * end_fraction)), size=(batch_size // 2, ), replace=False)
     # pos = np.random.randint(int(len(file_offsets) * start_fraction), int(len(file_offsets) * end_fraction), size=(batch_size // 2,))
     
-    starts = torch.from_numpy(file_offsets[pos, 0]).repeat_interleave(2, dim=0)
-    lengths = torch.from_numpy(file_offsets[pos, 1]).repeat_interleave(2, dim=0)
+    starts = np.repeat(file_offsets[pos, 0], 2)
+    lengths = np.repeat(file_offsets[pos, 1], 2)
     # starts = torch.randint(int(len(data) * start_fraction), int(len(data) * end_fraction), size=(batch_size, ))
     # lengths = torch.zeros_like(starts)
-    idxs = starts + torch.rand(lengths.shape) * lengths
+    idxs = starts + np.random.rand(lengths.shape) * lengths
     # idxs = torch.cat([torch.randint(start, start + length - n_samples, size=(2,)) for start, length in zip(starts, lengths)], dim=0)
-    return idxs.long()
+    return idxs.astype(np.int64)
     
 
 def get_batch(split='train', batch_size=batch_size):
