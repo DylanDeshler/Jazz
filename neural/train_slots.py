31df19383be29ffba997dbf525f4544603f6959a
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-17 15:07:44 -0500

    slots

--- neural/train_slots.py
+++ neural/train_slots.py
@@ -48,7 +48,7 @@ save_interval = 5000
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = False # disabled by default
 wandb_project = out_dir
