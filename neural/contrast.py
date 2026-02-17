08fb37c93a67a1a9372d1ca06913719253749d38
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-16 21:35:46 -0500

    contrast

--- neural/contrast.py
+++ neural/contrast.py
@@ -384,7 +384,7 @@ class Transformer(nn.Module):
     def info_nce_loss(self, features):
 
         # labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
-        labels = torch.arange(len(features)).repeat_interleave(2, dim=0)
+        labels = torch.arange(len(features) // 2).repeat_interleave(2, dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
         labels = labels.to(features.device)
 
