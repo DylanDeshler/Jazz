e26166d55293e294b98d59bac689161fecbc07bc
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-16 21:33:26 -0500

    contrast

--- neural/contrast.py
+++ neural/contrast.py
@@ -361,6 +361,7 @@ class Transformer(nn.Module):
         super().__init__()
         self.patch_size = patch_size
         self.head_dim = hidden_size // num_heads
+        self.temperature = 0.5
         
         self.to_mel = ToMel(sample_rate, n_fft, hop_length, n_mels)
         self.augment = SpecAugment()
@@ -375,9 +376,42 @@ class Transformer(nn.Module):
         self.norm = RMSNorm(hidden_size)
         self.fc = nn.Linear(hidden_size, hidden_size, bias=False)
         
-        self.criterion = losses.SelfSupervisedLoss(losses.NTXentLoss(temperature=0.5), symmetric=True)
+        # self.criterion = losses.SelfSupervisedLoss(losses.NTXentLoss(temperature=0.5), symmetric=True)
+        self.criterion = torch.nn.CrossEntropyLoss()
         
         self.initialize_weights()
+
+    def info_nce_loss(self, features):
+
+        # labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
+        labels = torch.arange(len(features)).repeat_interleave(2, dim=0)
+        labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+        labels = labels.to(self.args.device)
+
+        features = F.normalize(features, dim=1)
+
+        similarity_matrix = torch.matmul(features, features.T)
+        # assert similarity_matrix.shape == (
+        #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
+        # assert similarity_matrix.shape == labels.shape
+
+        # discard the main diagonal from both: labels and similarities matrix
+        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(features.device)
+        labels = labels[~mask].view(labels.shape[0], -1)
+        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+        # assert similarity_matrix.shape == labels.shape
+
+        # select and combine multiple positives
+        positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+
+        # select only the negatives the negatives
+        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+
+        logits = torch.cat([positives, negatives], dim=1)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(features.device)
+
+        logits = logits / self.temperature
+        return logits, labels
     
     def initialize_weights(self):
         self.apply(self._init_weights)
@@ -405,6 +439,7 @@ class Transformer(nn.Module):
         return self.to_mel(x)
     
     def _compute_loss(self, x):
+        return self.info_nce_loss(x)
         embs = x[::2]
         ref_embs = x[1::2]
         loss = self.criterion(embs, ref_embs)
