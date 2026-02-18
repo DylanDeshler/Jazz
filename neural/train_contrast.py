939d6810b966c4317e1266ea0bc14d4140a491dd
Dylan Deshler <dylan.deshler@gmail.com>
2026-02-18 01:31:07 -0500

    contrast

--- neural/train_contrast.py
+++ neural/train_contrast.py
@@ -457,8 +457,8 @@ while True:
 
     # evaluate the loss on train/val sets and write checkpoints
     if iter_num % eval_interval == 0 and master_process:
-        # losses = estimate_loss()
-        # print(f"iter {iter_num}: train loss {losses['train']:.6f}, val loss {losses['val']:.6f}")
+        losses = estimate_loss()
+        print(f"iter {iter_num}: train loss {losses['train']:.6f}, val loss {losses['val']:.6f}")
         
         if iter_num % sample_interval == 0 and master_process:
             model.eval()
