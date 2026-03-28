d990c16dde4b2ad888b6738cbddfa53eb8a909c1
Dylan Deshler <dylan.deshler@gmail.com>
2026-03-28 18:50:54 -0400

    fad

--- neural/train_FAD.py
+++ neural/train_FAD.py
@@ -183,7 +183,7 @@ def get_batch(split='train', batch_size=batch_size):
     for idx in idxs:
         wav, _ = librosa.load(idx, sr=sample_rate)
         beat_path = idx.replace('jazz_data_16000_full_clean', '').replace('.wav', '.beats')
-        url = idx.split('/')[-1].split('.')[0]
+        url = idx.split('/')[-2].split('.')[0]
         
         start = np.random.randint(len(wav) - n_samples)
         timestamps = read_beat_timestamps(beat_path)
