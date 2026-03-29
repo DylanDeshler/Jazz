5a7eae0e6441d7403b4a296e2408f33e371b1200
Dylan Deshler <dylan.deshler@gmail.com>
2026-03-28 20:04:17 -0400

    fad

--- neural/fad.py
+++ neural/fad.py
@@ -203,5 +203,7 @@ class MultiTaskFAD(nn.Module):
             'inst': loss_inst,
             'label': loss_label
         }
+        for k, v in outputs['loss'].items():
+            print(k, v)
         
         return outputs
