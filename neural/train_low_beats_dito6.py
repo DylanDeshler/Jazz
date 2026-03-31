58b4589f507c700e8b5bc4ac8608ff0a4ee67b20
Dylan Deshler <dylan.deshler@gmail.com>
2026-03-31 13:28:42 -0400

    dito

--- neural/train_low_beats_dito6.py
+++ neural/train_low_beats_dito6.py
@@ -234,11 +234,12 @@ import librosa
 paths = glob.glob('/home/dylan.d/research/music/Jazz/jazz_data_16000_full_clean_measures/*.wav')
 with open('/home/dylan.d/research/music/Jazz/valid_files_by_bpm.json', 'r') as f:
     beat_paths = json.load(f)
+beat_paths = ['/home/dylan.d/research/music/Jazz/jazz_data_16000_full_clean_beats' + path for path in beat_paths]
 print(len(paths), len(beat_paths))
 print(sorted(paths)[0])
 print(sorted(beat_paths)[0])
-paths = [path.replace('jazz_data_16000_full_clean_measures', 'jazz_data_16000_full_clean_beats').replace('.wav', '.beats') for path in paths if path in beat_paths]
-print(len(paths))
+# paths = [path.replace('jazz_data_16000_full_clean_measures', 'jazz_data_16000_full_clean_beats').replace('.wav', '.beats') for path in paths if path in beat_paths]
+# print(len(paths))
 # paths = [path for path in paths if os.path.exists(path)]
 # print(len(paths))
 wavs = []
