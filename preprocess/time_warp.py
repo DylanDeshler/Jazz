9e6b315f418af3713af8082a155f411c0d05654f
Dylan Deshler <dylan.deshler@gmail.com>
2026-03-31 01:20:09 -0400

    warp

--- preprocess/time_warp.py
+++ preprocess/time_warp.py
@@ -190,10 +190,10 @@ def generate_audio_measures(paths):
     if np.mean(instant_bpms) < 40 or np.mean(instant_bpms) > 330:
         return None
     
-    print(np.stack(audios, axis=0).shape)
+    print(np.concatenate(audios, axis=0).shape)
     sf.write(
         file=out_path, 
-        data=np.stack(audios, axis=0), 
+        data=np.concatenate(audios, axis=0), 
         samplerate=TARGET_SR,
         subtype='PCM_16'
     )
