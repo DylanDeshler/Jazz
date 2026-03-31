f90b978590f05cdda0ac73b21ec742ae565b6be0
Dylan Deshler <dylan.deshler@gmail.com>
2026-03-30 20:56:14 -0400

    dito

--- neural/train_low_dito6.py
+++ neural/train_low_dito6.py
@@ -345,7 +345,7 @@ if wandb_log and master_process:
     import wandb
     if init_from == 'scratch':
         wandb.init(project=wandb_project, name=wandb_run_name, config=config)
-    elif init_from == 'reume':
+    elif init_from == 'resume':
         wandb.init(project=wandb_project, name=wandb_run_name, config=config, id='lyps4qwv', resume='must')
 
 # training loop
