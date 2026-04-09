37a495f1e6d948ce6da20c3efb3fc41cd3c63f52
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-09 03:47:54 -0400

    dito

--- neural/train_low_beats_dito6.py
+++ neural/train_low_beats_dito6.py
@@ -40,7 +40,7 @@ import glob
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
-out_dir = 'tokenizer_low_measures_2std_subset'
+out_dir = 'tokenizer_low_measures_fix_subset'
 eval_interval = 5000
 log_interval = 100
 eval_iters = 600
