8cd364df9782295a86f096d9ee4d686363ceac13
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-09 22:11:55 -0400

    dito

--- neural/train_low_dito6.py
+++ neural/train_low_dito6.py
@@ -44,7 +44,7 @@ log_interval = 100
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
 wandb_project = out_dir #'zinc20++'
@@ -360,7 +360,7 @@ if wandb_log and master_process:
     if init_from == 'scratch':
         wandb.init(project=wandb_project, name=wandb_run_name, config=config)
     elif init_from == 'resume':
-        wandb.init(project=wandb_project, name=wandb_run_name, config=config, id='vo20wfqe', resume='must')
+        wandb.init(project=wandb_project, name=wandb_run_name, config=config, id='6epdpb5n', resume='must')
 
 # training loop
 
