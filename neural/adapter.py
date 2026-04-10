62514faa97120ea61e8a606b78f1e0ed2a8dc1a7
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-10 18:35:51 -0400

    adapter

--- neural/adapter.py
+++ neural/adapter.py
@@ -282,7 +282,8 @@ class SequenceDecoder(nn.Module):
         # zero out c_proj weights in all blocks
         for block in self.blocks:
             nn.init.zeros_(block.mlp.w3.weight)
-            nn.init.zeros_(block.attn.proj.weight)
+            nn.init.zeros_(block.sa.proj.weight)
+            nn.init.zeros_(block.ca.proj.weight)
     
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
