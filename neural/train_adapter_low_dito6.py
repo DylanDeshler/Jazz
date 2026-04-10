66940f17db717831040b433c7c4c3e524b8ac1d1
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-10 17:55:56 -0400

    adapter

--- neural/train_adapter_low_dito6.py
+++ neural/train_adapter_low_dito6.py
@@ -47,7 +47,7 @@ eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = True # disabled by default
+wandb_log = False # disabled by default
 wandb_project = out_dir #'zinc20++'
 wandb_run_name = 'llama' + str(time.time())
 # data
@@ -340,6 +340,7 @@ def get_batch(split='train', batch_size=batch_size):
     indices = torch.arange(max_seq_len, device=x.device).unsqueeze(0)
     lengths = (torch.from_numpy(np.asarray(lengths)).unsqueeze(1).to(device) + encoder_ratios - 1) // encoder_ratios
     mask = indices >= lengths
+    print(mask.shape)
     
     return x, mask
 
