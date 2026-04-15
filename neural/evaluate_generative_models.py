b49193624a19ee8fb895ab33b9d4d365340320f8
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-15 18:32:12 -0400

    val

--- neural/evaluate_generative_models.py
+++ neural/evaluate_generative_models.py
@@ -272,8 +272,7 @@ with torch.no_grad():
                 max_T = latent_lengths.max().item()
                 indices = torch.arange(max_T, device=device).view(1, 1, max_T)
                 lengths_expanded = latent_lengths.unsqueeze(-1)
-                valid_mask = indices < lengths_expanded
-                mask = valid_mask.unsqueeze(1).unsqueeze(2)
+                mask = indices < lengths_expanded
                 shape = (batch_size * n_chunks, 1, max_T)
                 
                 y2 = y2.view(batch_size * n_chunks, vae_embed_dim, spatial_window)
