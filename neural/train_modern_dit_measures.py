3cf55911ff3b0563e1f068d02706438d2423792f
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-16 20:22:11 -0400

    dit

--- neural/train_modern_dit_measures.py
+++ neural/train_modern_dit_measures.py
@@ -48,7 +48,7 @@ log_interval = 100
 save_interval = 5000
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
-always_save_checkpoint = False # if True, always save a checkpoint after each eval
+always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
@@ -336,11 +336,11 @@ while True:
         losses = estimate_loss()
         print(f"iter {iter_num}: train loss {losses['train']:.6f}, val loss {losses['val']:.6f}")
         
-        # if iter_num % sample_interval == 0 and master_process:
-        #     model.eval()
-        #     with ctx:
-        #         save_samples(iter_num)
-        #     model.train()
+        if iter_num % sample_interval == 0 and master_process:
+            model.eval()
+            with ctx:
+                save_samples(iter_num)
+            model.train()
         
         if wandb_log:
             wandb.log({
@@ -351,20 +351,32 @@ while True:
                 "mfu": running_mfu*100, # convert to percentage
                 "tokens": tokens_trained,
             })
-        if losses['val'] < best_val_loss or always_save_checkpoint:
+        if iter_num > 0 and losses['val'] < best_val_loss:
             best_val_loss = losses['val']
-            if iter_num > 0:
-                checkpoint = {
-                    'model': raw_model.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'model_args': model_args,
-                    'iter_num': iter_num,
-                    'best_val_loss': best_val_loss,
-                    'config': config,
-                    'tokens': tokens_trained,
-                }
-                print(f"saving checkpoint to {out_dir}")
-                torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
+            checkpoint = {
+                'model': raw_model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'model_args': model_args,
+                'iter_num': iter_num,
+                'val_loss': best_val_loss,
+                'best_val_loss': best_val_loss,
+                'config': config,
+                'tokens': tokens_trained,
+            }
+            torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
+            print(f"saving new best checkpoint to {out_dir}")
+        if iter_num > 0 and always_save_checkpoint:
+            checkpoint = {
+                'model': raw_model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'model_args': model_args,
+                'iter_num': iter_num,
+                'val_loss': losses['val'],
+                'best_val_loss': best_val_loss,
+                'config': config,
+                'tokens': tokens_trained,
+            }
+            torch.save(checkpoint, os.path.join(out_dir, f'ckpt_{iter_num}.pt'))
     
     if eval_only:
         break
