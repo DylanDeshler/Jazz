66bb5bda1afc205ffd51ac4c1edf6d55e28975d1
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-16 21:05:46 -0400

    dit

--- neural/train_modern_dit_measures.py
+++ neural/train_modern_dit_measures.py
@@ -30,7 +30,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 from einops import rearrange
 
-from diffusion_forcing import UnconditionalModernDiT_medium as net
+from diffusion_forcing import UnconditionalModernDiT_small as net
 from dito import DiToV5 as Tokenizer
 from adapter import InvertibleAdapter
 import soundfile as sf
@@ -41,7 +41,7 @@ import pyrubberband as pyrb
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
-out_dir = 'UnconditionalModernDiT_medium_24576_subset_adapter_32chunks'
+out_dir = 'UnconditionalModernDiT_small_24576_subset_adapter_32chunks'
 eval_interval = 5000
 sample_interval = 5000
 log_interval = 100
