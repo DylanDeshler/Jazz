2e9ceea1fafc54e7f979b54fafbea732f2f88b49
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-20 02:40:27 -0400

    adapter

--- neural/train_adapter_low_dito6.py
+++ neural/train_adapter_low_dito6.py
@@ -53,7 +53,7 @@ wandb_run_name = 'llama' + str(time.time())
 # data
 dataset = ''
 gradient_accumulation_steps = 16 # used to simulate larger batch sizes
-batch_size = 16 # if gradient_accumulation_steps > 1, this is the micro-batch size
+batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
 # model
 rate = 16000
 n_samples = 24576
