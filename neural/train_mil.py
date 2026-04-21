b396c9eaf363668a89344de85ce89ff68e62fa13
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-20 23:49:04 -0400

    mil

--- neural/train_mil.py
+++ neural/train_mil.py
@@ -220,8 +220,8 @@ def get_batch(split='train'):
     inst = []
     for idx in idxs:
         # wav = wavs[idx]
-        # wav = librosa.load(paths[idx], sr=None)[0]
-        wav = torchaudio.load(paths[idx])[0]
+        wav = librosa.load(paths[idx], sr=None)[0]
+        # wav = torchaudio.load(paths[idx])[0]
         url = paths[idx].split('/')[-1].split('.')[0]
         
         start = np.random.randint(len(wav) - n_samples)
