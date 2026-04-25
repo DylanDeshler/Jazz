7274cea7bd15df0c353930f65836b3818e560675
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-25 17:19:40 -0400

    mil

--- neural/train_mil_unet.py
+++ neural/train_mil_unet.py
@@ -55,7 +55,7 @@ n_mels = 192
 time_length = 32
 frequency_length = 64
 # adamw optimizer
-learning_rate = 4e-3 * math.sqrt(batch_size / 4096) # max learning rate
+learning_rate = 4e-3 * math.sqrt(batch_size * gradient_accumulation_steps / 4096) # max learning rate
 max_iters = 1000000 # total number of training iterations
 weight_decay = 1e-2
 beta1 = 0.9
