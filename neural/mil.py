28c078e65e9d1c9df6643683aa82cac9bb7d7e25
Dylan Deshler <dylan.deshler@gmail.com>
2026-04-26 21:21:38 -0400

    mil

--- neural/mil.py
+++ neural/mil.py
@@ -680,9 +680,9 @@ class UNet(nn.Module):
         
         nn.init.zeros_(self.proj.weight)
         # zero out c_proj weights in all blocks
-        for block in self.blocks:
-            nn.init.zeros_(block.mlp.w3.weight)
-            nn.init.zeros_(block.attn.proj.weight)
+        # for block in self.blocks:
+        #     nn.init.zeros_(block.mlp.w3.weight)
+        #     nn.init.zeros_(block.attn.proj.weight)
 
     def forward(self, x, targets=None):
         B, C, L = x.shape
