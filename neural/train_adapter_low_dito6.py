559b340375b28cbf34f48e0bc5a7c0a5a5b2845f
Dylan Deshler <dylan.deshler@gmail.com>
2026-05-13 04:37:28 -0400

    adapter

--- neural/train_adapter_low_dito6.py
+++ neural/train_adapter_low_dito6.py
@@ -73,7 +73,7 @@ beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
-decay_lr = True # whether to decay the learning rate
+decay_lr = False # whether to decay the learning rate
 warmup_iters = 5000 # how many steps to warm up for
 lr_decay_iters = max_iters # should be ~= max_iters per Chinchilla
 min_lr = learning_rate / 10 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
