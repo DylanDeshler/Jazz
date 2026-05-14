03cc56d12b7e01a211377ac97a9e85a223361931
Dylan Deshler <dylan.deshler@gmail.com>
2026-05-13 21:46:19 -0400

    dit

--- neural/train_modern_fancy_dit_measures.py
+++ neural/train_modern_fancy_dit_measures.py
@@ -53,7 +53,7 @@ save_interval = 5000
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
 wandb_project = out_dir
@@ -423,7 +423,7 @@ class EMAModel:
 if wandb_log and master_process:
     import wandb
     if init_from == 'resume':
-        wandb.init(project=wandb_project, name=wandb_run_name, id='hdcipov7', config=config)
+        wandb.init(project=wandb_project, name=wandb_run_name, id='j2ajnk5e', config=config)
     else:
         wandb.init(project=wandb_project, name=wandb_run_name, config=config)
 
