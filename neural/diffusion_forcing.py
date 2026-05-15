dd559e236516688312a27bd93992d94fcd52b3f9
Dylan Deshler <dylan.deshler@gmail.com>
2026-05-15 18:41:27 -0400

    dit

--- neural/diffusion_forcing.py
+++ neural/diffusion_forcing.py
@@ -2054,10 +2054,8 @@ class MetaConditionalModernDiTV2(nn.Module):
         
         if self.stage == 2:
             c = torch.cat([chroma, rms.unsqueeze(-1), density.unsqueeze(-1), zcr.unsqueeze(-1), flatness.unsqueeze(-1)], dim=-1)
-            print(c.shape)
             c = c.unsqueeze(2).repeat(1, 1, N, 1)
-            print(c.shape)
-            c = rearrange(x, 'b t n c -> (b t) c n')
+            c = rearrange(c, 'b t n c -> (b t) c n')
             c = self.local_embedder(c)
             x = x + c
         x = rearrange(x, '(b t) c n -> b (t n) c', b=B, t=T)
