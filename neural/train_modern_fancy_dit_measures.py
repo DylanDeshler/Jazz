fa85e470bd2fb73cb6d1cde9fc0f0efab4f11880
Dylan Deshler <dylan.deshler@gmail.com>
2026-05-15 01:18:13 -0400

    dit

--- neural/train_modern_fancy_dit_measures.py
+++ neural/train_modern_fancy_dit_measures.py
@@ -380,9 +380,9 @@ def predict_measures(gen_shape, net_kwargs, uncond_net_kwargs, n_steps, guidance
     
     out = []
     for i in range(gen_shape[0]):
-        temp = y[i*n_chunks][:min(int(target_samples[i*n_chunks]), max_len)]
+        temp = y[i*n_chunks][:min(int(target_samples[i][0]), max_len)]
         for j in range(1, n_chunks):
-            temp = crossfade_segments(temp, y[i*n_chunks+j][:min(int(target_samples[i*n_chunks+j]), max_len)], sample_rate=16000, crossfade_ms=20)
+            temp = crossfade_segments(temp, y[i*n_chunks+j][:min(int(target_samples[i][j]), max_len)], sample_rate=16000, crossfade_ms=20)
         out.append(temp.astype(np.float32))
     
     return out
@@ -415,9 +415,9 @@ def decode_latents(y, bpm, n_steps, decoder_noise=None):
     
     out = []
     for i in range(bpm.shape[0]):
-        temp = y[i*n_chunks][:min(int(target_samples[i*n_chunks]), max_len)]
+        temp = y[i*n_chunks][:min(int(target_samples[i][0]), max_len)]
         for j in range(1, n_chunks):
-            temp = crossfade_segments(temp, y[i*n_chunks+j][:min(int(target_samples[i*n_chunks+j]), max_len)], sample_rate=16000, crossfade_ms=20)
+            temp = crossfade_segments(temp, y[i*n_chunks+j][:min(int(target_samples[i][j]), max_len)], sample_rate=16000, crossfade_ms=20)
         out.append(temp.astype(np.float32))
     
     return out
