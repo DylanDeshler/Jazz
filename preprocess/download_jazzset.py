0e14d2105a0bd378cdf34cb32a45bfb39c06252b
Dylan Deshler <dylan.deshler@gmail.com>
2026-05-26 15:52:37 -0400

    pre

--- preprocess/download_jazzset.py
+++ preprocess/download_jazzset.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 # Configuration
 rate = 16000
 out_dir = '/data/wavs'
-os.makedirs(out_dir, exist_ok=True)
+os.makedirs(out_dir, exist_ok=False)
 MAX_WORKERS = 40  # Number of simultaneous downloads/processes
 
 def process_card(card):
