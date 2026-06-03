9837c479f44ddd1dd164cb98806161c786bd19e6
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-03 00:55:33 -0400

    data

--- neural/generate_captions_dataset.py
+++ neural/generate_captions_dataset.py
@@ -34,7 +34,7 @@ embedding_memmap = np.memmap(
     os.path.join(os.path.dirname(__file__), f'{out_prefix}.bin'), 
     dtype=np.float32, 
     mode='w+', 
-    shape=(len(captions), 3, max_tokens, model.config.d_model)
+    shape=(len(captions) + 1, 3, max_tokens, model.config.d_model)
 )
 
 from collections import defaultdict
@@ -65,33 +65,38 @@ for data_dict in tqdm(captions, desc='Calculating Token Lengths'):
 for k, v in lengths.items():
     print(f'[{k} token stats] min: {np.min(v)} mean: {np.mean(v)} std: {np.std(v)} max: {np.max(v)}')
 
-if True:
+inputs = tokenizer(
+    [''],
+    padding="max_length",
+    truncation=True,
+    max_length=max_tokens,
+    return_tensors="pt"
+).to(device)
 
-    write_idx = 0
-    write_paths = []
+outputs = model(**inputs)
+embedding_memmap[0] = outputs.last_hidden_state.cpu().numpy()
 
-    all_codes = []
-    with torch.no_grad():
-        for idx, data_dict in enumerate(tqdm(captions)):
-            path = data_dict['file_path']
-            data = data_dict.get('llm_output', [])
-            if isinstance(data, list):
-                data = {'short_caption': '', 'medium_caption': '', 'long_caption': ''}
-            
-            short = data.get('short_caption', '')
-            medium = data.get('medium_caption', '')
-            long = data.get('long_caption', '')
-            
-            texts = [short, medium, long]
-            
-            inputs = tokenizer(
-                texts,
-                padding="max_length",
-                truncation=True,
-                max_length=max_tokens,
-                return_tensors="pt"
-            ).to(device)
-            
-            outputs = model(**inputs)
-            embedding_memmap[idx] = outputs.last_hidden_state.cpu().numpy()
+with torch.no_grad():
+    for idx, data_dict in enumerate(tqdm(captions)):
+        path = data_dict['file_path']
+        data = data_dict.get('llm_output', [])
+        if isinstance(data, list):
+            data = {'short_caption': '', 'medium_caption': '', 'long_caption': ''}
+        
+        short = data.get('short_caption', '')
+        medium = data.get('medium_caption', '')
+        long = data.get('long_caption', '')
+        
+        texts = [short, medium, long]
+        
+        inputs = tokenizer(
+            texts,
+            padding="max_length",
+            truncation=True,
+            max_length=max_tokens,
+            return_tensors="pt"
+        ).to(device)
+        
+        outputs = model(**inputs)
+        embedding_memmap[idx + 1] = outputs.last_hidden_state.cpu().numpy()
 embedding_memmap.flush()
