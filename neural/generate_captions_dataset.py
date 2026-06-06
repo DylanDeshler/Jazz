cc4e5d152aaec91563dd3d73b11ded73846ada8b
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-06 13:16:51 -0400

    captions

--- neural/generate_captions_dataset.py
+++ neural/generate_captions_dataset.py
@@ -12,7 +12,7 @@ import torch
 
 from transformers import T5Tokenizer, T5EncoderModel
 
-device = torch.device('cuda:3')
+device = torch.device('cuda:0')
 
 batch_size = 32
 total_write_batches = 16
