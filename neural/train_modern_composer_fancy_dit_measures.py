717a21176e2c37034a7bada1d1747070ac8c33ea
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-08 12:15:05 -0400

    dit

--- neural/train_modern_composer_fancy_dit_measures.py
+++ neural/train_modern_composer_fancy_dit_measures.py
@@ -190,7 +190,8 @@ def get_batch(split='train', batch_size=batch_size):
     idx_matrix = np.minimum(idx_matrix, stops[:, None])
     t4 = time.time()
     
-    text = torch.from_numpy(np.stack([data[i, j, k, :n_text_tokens] for i, j, k in zip(song_idxs, caption_idxs, caption_vars)], axis=0)).pin_memory().to(device, non_blocking=True)
+    # text = torch.from_numpy(np.stack([data[i, j, k, :n_text_tokens] for i, j, k in zip(song_idxs, caption_idxs, caption_vars)], axis=0)).pin_memory().to(device, non_blocking=True)
+    text = torch.from_numpy(data[song_idxs, caption_idxs, caption_vars, :n_text_tokens]).pin_memory().to(device, non_blocking=True)
     t5 = time.time()
     
     style = torch.from_numpy(style[idx_matrix]).pin_memory().to(device, non_blocking=True)
