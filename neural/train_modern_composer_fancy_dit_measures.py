ad02fc974642c8a1a6c4564d29d9cceefa3f5c19
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-08 20:00:53 -0400

    dit

--- neural/train_modern_composer_fancy_dit_measures.py
+++ neural/train_modern_composer_fancy_dit_measures.py
@@ -194,7 +194,7 @@ def get_batch(split='train', batch_size=batch_size):
     # text = torch.from_numpy(data[song_idxs, caption_idxs, caption_vars]).pin_memory().to(device, non_blocking=True)
     local_data = data[song_idxs[0]:song_idxs[0] + batch_size]
     print(local_data.shape)
-    text = local_data[:, caption_idxs, caption_vars]
+    text = local_data[np.arange(batch_size), caption_idxs, caption_vars]
     print(text.shape)
     text = torch.from_numpy(text).pin_memory().to(device, non_blocking=True)
     t5 = time.time()
