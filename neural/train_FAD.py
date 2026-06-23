c0ada8e2f644d9cabe380a9967808cafa7cda0b8
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-23 17:12:03 -0400

    fad

--- neural/train_FAD.py
+++ neural/train_FAD.py
@@ -277,7 +277,7 @@ def get_batch(split='train'):
     label = []
     inst = []
     for start, idx in zip(starts, idxs):
-        wav, file_sample_rate = sf.read(paths[idx], start=start, frames=n_samples)
+        wav, file_sample_rate = sf.read(paths[idx], start=start, stop=start+n_samples)
         print(wav.shape, start, durations[idx], durations[idx] - start - n_samples)
         beat_path = os.path.join('/data/beats', os.path.basename(paths[idx]))
         url = paths[idx].split('/')[-1].split('.')[0]
