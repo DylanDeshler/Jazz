ceb6f62838e615a6cb09d6e70da13acd3741028c
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-23 22:56:46 -0400

    dit

--- neural/train_modern_dit.py
+++ neural/train_modern_dit.py
@@ -49,7 +49,7 @@ parser.add_argument(
     '--axis',
     type=str,
     required=True,
-    choices=['width', 'depth'],
+    choices=['width', 'depth', 'chunks'],
     help="Specify width or depth scaling experiment."
 )
 parser.add_argument(
@@ -87,7 +87,10 @@ net_map = {
     'D4': UnconditionalModernDiT_smedium_D4,
     'D5': UnconditionalModernDiT_smedium_D5
 }
-level = f'{args.axis[0].upper()}{args.level[1]}'
+if args.axis == 'chunks':
+    level = 'D3' # W4 and D3 are identical
+else:
+    level = f'{args.axis[0].upper()}{args.level[1]}'
 net = net_map[level]
 n_chunks = args.n_chunks
 print(f"Begining training for {level} on {args.device}")
@@ -103,7 +106,7 @@ save_interval = 5000
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
 wandb_project = out_dir
@@ -119,7 +122,6 @@ if int(level[1]) > 4:
 # model
 patch_size = 2
 gradient_checkpointing = False
-# set to match with measures using n_chunks=24, spatial_window=64
 spatial_window = 48
 max_seq_len = spatial_window * n_chunks
 vae_embed_dim = 16
@@ -361,7 +363,7 @@ if wandb_log and master_process:
     if init_from == 'scratch':
         wandb.init(project=wandb_project, name=wandb_run_name, config=config)
     elif init_from == 'resume':
-        wandb.init(project=wandb_project, name=wandb_run_name, config=config, id='to9c2olw', resume='must')
+        wandb.init(project=wandb_project, name=wandb_run_name, config=config, id='lrobce9l', resume='must')
 
 # training loop
 X = get_batch('train') # fetch the very first batch
