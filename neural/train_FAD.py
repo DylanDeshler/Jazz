5ef0a6bf9fcad2f81a737c1bade251c2da2b3b10
Dylan Deshler <dylan.deshler@gmail.com>
2026-06-23 22:53:12 -0400

    fad

--- neural/train_FAD.py
+++ neural/train_FAD.py
@@ -31,7 +31,7 @@ save_interval = 5000
 eval_iters = 600
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default
 wandb_project = out_dir
@@ -417,7 +417,10 @@ model.train()
 
 if wandb_log and master_process:
     import wandb
-    wandb.init(project=wandb_project, name=wandb_run_name, config=config)
+    if init_from == 'scratch':
+        wandb.init(project=wandb_project, name=wandb_run_name, config=config)
+    elif init_from == 'resume':
+        wandb.init(project=wandb_project, name=wandb_run_name, config=config, id='a1k9o85o', resume='must')
 
 # training loop
 X, targets, masks = get_batch('train') # fetch the very first batch
