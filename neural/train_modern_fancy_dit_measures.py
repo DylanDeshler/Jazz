ca8fd52fbae577f17233f054128b88da1b1af665
Dylan Deshler <dylan.deshler@gmail.com>
2026-08-04 22:51:32 -0400

    dit

--- neural/train_modern_fancy_dit_measures.py
+++ neural/train_modern_fancy_dit_measures.py
@@ -56,13 +56,13 @@ wandb_project = out_dir
 wandb_run_name = str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 1
-batch_size = 128
+gradient_accumulation_steps = 2
+batch_size = 64
 TARGET_SIG = 4
 TARGET_BPM = 60 * TARGET_SIG / (24576 / 16000)
 # model
 patch_size = 2
-gradient_checkpointing = False
+gradient_checkpointing = True
 spatial_window = 64
 n_chunks = 24
 max_seq_len = spatial_window * n_chunks
