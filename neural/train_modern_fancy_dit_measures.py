8ebea57a531449990fb2f99666877ffe4df1798e
Dylan Deshler <dylan.deshler@gmail.com>
2026-08-04 22:50:41 -0400

    dit

--- neural/train_modern_fancy_dit_measures.py
+++ neural/train_modern_fancy_dit_measures.py
@@ -56,8 +56,8 @@ wandb_project = out_dir
 wandb_run_name = str(time.time())
 # data
 dataset = ''
-gradient_accumulation_steps = 2
-batch_size = 64
+gradient_accumulation_steps = 1
+batch_size = 128
 TARGET_SIG = 4
 TARGET_BPM = 60 * TARGET_SIG / (24576 / 16000)
 # model
